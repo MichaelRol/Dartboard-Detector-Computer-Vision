54297919d7ee441b6dc8d2af66fa271910d53557
MichaelRol <michael.rollins@hotmail.co.uk>
2018-12-02 21:00:40 +0000

    HI

--- Subtask3/dart.cpp
+++ Subtask3/dart.cpp
@@ -57,7 +57,8 @@ int main( int argc, const char** argv ) {
 	for (int x = 0; x < 20; x++) {
 		for (int y = 0; y < 20; y++) {
 			if (boards[x*20+y] > 20) {
-				cout << "X: " << x << " Y: " << y << endl;
+				// cout << "X: " << x << " Y: " << y << endl;
+				cout << boards[x*20+y];
 			}
 		}
 	}
