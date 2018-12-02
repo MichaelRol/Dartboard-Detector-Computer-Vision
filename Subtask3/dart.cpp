f52e5ca5a69e728b0e083b65a4911bbb69229454
MichaelRol <michael.rollins@hotmail.co.uk>
2018-12-02 18:27:41 +0000

    needed floats and rounding and dumb as shit like that

--- Subtask3/dart.cpp
+++ Subtask3/dart.cpp
@@ -99,13 +99,13 @@ Mat detectBoards(Mat originalImage, Mat houghSpace) {
 
 				int r0 = linearray[line*2];
 				int r1 = linearray[restoflines*2];
-				int a0 = cos(degrees0*pi/180);
-				int a1 = cos(degrees1*pi/180);
-				int b0 = sin(degrees0*pi/180);
-				int b1 = sin(degrees1*pi/180);
+				float a1 = cos(degrees1*pi/180);
+				float b0 = sin(degrees0*pi/180);
+				float a0 = cos(degrees0*pi/180);
+				float b1 = sin(degrees1*pi/180);
 
-				int x = (r0*b1 - b0*r1)/(a0*b1 - b0*a1);
-				int y = (a0*r1 - r0*a1)/(a0*b1 - b0*a1);
+				int x = round((r0*b1 - b0*r1)/(a0*b1 - b0*a1));
+				int y = round((a0*r1 - r0*a1)/(a0*b1 - b0*a1));
 				cout << x << " " << y << endl;
 			}
 		}
